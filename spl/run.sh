b6be99fa717f0dc067d71fed2a22a22ce9de0b6f
steward-fu <g9313716@yuntech.edu.tw>
2019-01-04 16:54:16 +0800

    updated flashable image for hardware mod user

--- spl/run.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-sudo rm flashimg.bin
-sudo dd if=/dev/zero of=flashimg.bin bs=1M count=8
-sudo dd if=u-boot-sunxi-with-spl.bin of=flashimg.bin bs=1K conv=notrunc
-sudo dd if=suniv-f1c500s-miyoo.dtb of=flashimg.bin bs=1K seek=1024 conv=notrunc
-sudo dd if=zImage of=flashimg.bin bs=1K seek=1088 conv=notrunc
-sudo sunxi-fel -p spiflash-write 0 flashimg.bin
